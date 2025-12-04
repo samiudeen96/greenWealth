9469d6f48ec057cbcc22bbde9f0f823e71f0a7a4
samiudeen96 <samiudeen96amm@gmail.com>
2025-12-04 18:52:44 +0400

    update

--- src/app/(pages)/shop/page.jsx
+++ src/app/(pages)/shop/page.jsx
@@ -24,7 +24,7 @@ const Shop = () => {
                 </div>
 
                 <div className="py-16">
-                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+                  <div className="">
                     {data?.map((item)=>(
                       <ProductCards item={item} key={item.id} />
                     ))}
