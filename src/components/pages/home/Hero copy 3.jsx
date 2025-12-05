d39ce6c77e6a0b1f42c8789c4dc4afe4b0347255
samiudeen96 <samiudeen96amm@gmail.com>
2025-12-05 08:34:21 +0400

    update

--- src/components/pages/home/Hero copy 3.jsx
+++ src/components/pages/home/Hero copy 3.jsx
@@ -46,100 +46,48 @@ const Hero = () => {
             y: device === "mobile" ? "0vh" : "-28vh",
             duration: 0.5,
             ease: "power1.inOut",
+          })
+
+          //   .fromTo(
+          //   ".img",
+          //   { opacity: 0, scale: 0.8, y: "50%" },
+          //   { opacity: 1, scale: 1, y: 0, duration:0.5, ease: "power1.out" }
+          // )
+
+          gsap.to(".productWrapperRef", {
+            y:
+              device === "mobile"
+                ? "90%"
+                : device === "tablet"
+                ? "-20%"
+                : "50%",
+            scale: device === "mobile" ? 0.8 : device === "tablet" ? 0.8 : 0.6,
+            ease: "power1.inOut",
+            scrollTrigger: {
+              trigger: ".heroSec1",
+              start: "top-=60px top+=70px",
+              end: "bottom+=100 center",
+              scrub: true,
+              // markers: true
+            },
+          })
+          gsap.to(productWrapperRef.current, {
+            y:
+              device === "mobile"
+                ? "-8%"
+                : device === "tablet"
+                ? "-20%"
+                : "-50%",
+            x: device === "mobile" ? 0 : device === "tablet" ? 150 : 330,
+            ease: "power1.out",
+            scrollTrigger: {
+              trigger: ".heroSec3",
+              start: "top bottom", // When top of the content hits bottom of the viewport
+              end: "top center",
+              scrub: true,
+              // markers: true
+            },
           });
-
-        //   .fromTo(
-        //   ".img",
-        //   { opacity: 0, scale: 0.8, y: "50%" },
-        //   { opacity: 1, scale: 1, y: 0, duration:0.5, ease: "power1.out" }
-        // )
-
-        // -------------------------------
-        // PIN FIRST PRODUCT SECTION
-        // -------------------------------
-        ScrollTrigger.create({
-          trigger: ".product-pin-section",
-          start: "center-=52px center",
-          endTrigger: ".sectionEnd",
-          end: "bottom-=170 top",
-          pin: true,
-          pinType: "transform",
-          pinSpacing: false,
-        });
-
-        // gsap.to(productWrapperRef.current, {
-        //   y: device === "mobile" ? "90%" : device === "tablet" ? "-20%" : "50%",
-        //   scale: device === "mobile" ? 0.8 : device === "tablet" ? 0.8 : 0.6,
-        //   ease: "power1.inOut",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec1",
-        //     start: "top-=60px top+=70px",
-        //     end: "bottom+=100 center",
-        //     scrub: true,
-        //     // markers: true
-        //   },
-        // });
-
-        // gsap.to(productWrapperRef.current, {
-        //   y:
-        //     device === "mobile" ? "-8%" : device === "tablet" ? "-20%" : "-50%",
-        //   x: device === "mobile" ? 0 : device === "tablet" ? 150 : 330,
-        //   scale: 1,
-        //   ease: "power1.out",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec3",
-        //     start: "top bottom", // When top of the content hits bottom of the viewport
-        //     end: "top center",
-        //     scrub: true,
-        //     // markers: true
-        //   },
-        // });
-
-        // Use keyframes (smoothest)
-        // GSAP keyframes automatically blend the values:
-        // ✔ No jump
-        // ✔ Automatically calculates smooth transitions
-        // ✔ Best for multi-step transformations
-        // ❗ Why the scale jumps
-
-        // Because:
-        // Your first animation finishes too early
-        // GSAP has no animation active for a long scroll stretch
-        // When the second animation finally starts
-        // Scale jumps immediately to match the next .to() target
-        // The fix is: stretch animation timing across scroll distance.
-
-        Benefits: gsap.to(productWrapperRef.current, {
-          keyframes: [
-            {
-              y:
-                device === "mobile"
-                  ? "90%"
-                  : device === "tablet"
-                  ? "-20%"
-                  : "50%",
-              scale:
-                device === "mobile" ? 0.8 : device === "tablet" ? 0.8 : 0.6,
-            },
-            {
-              y:
-                device === "mobile"
-                  ? "-8%"
-                  : device === "tablet"
-                  ? "-20%"
-                  : "-50%",
-              x: device === "mobile" ? 0 : device === "tablet" ? 150 : 330,
-              scale: 1,
-            },
-          ],
-          ease: "none",
-          scrollTrigger: {
-            trigger: ".heroMainWrapper",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: true,
-          },
-        });
 
         // -------------------------------
         // CHARS BLUR Animation
@@ -165,6 +113,19 @@ const Hero = () => {
         );
 
         // -------------------------------
+        // PIN FIRST PRODUCT SECTION
+        // -------------------------------
+        ScrollTrigger.create({
+          trigger: ".product-pin-section",
+          start: "center-=52px center",
+          endTrigger: ".sectionEnd",
+          end: "bottom-=170 top",
+          pin: true,
+          pinType: "transform",
+          pinSpacing: false,
+        });
+
+        // -------------------------------
         // PIN EACH SCROLL SECTION AFTER
         // -------------------------------
         scrollRefs.current.forEach((section) => {
@@ -175,7 +136,7 @@ const Hero = () => {
             pin: true,
             // pinType: "transform",
             // pinSpacing: scrollRefs.current.length === 5 ? true : false,
-            pinSpacing: false,
+            pinSpacing: false
           });
         });
 
@@ -185,81 +146,81 @@ const Hero = () => {
 
         // gsap.timeline()
 
-        // .to(".productWrapperRef", {
-        //   y:
-        //     device === "mobile"
-        //       ? "90%"
-        //       : device === "tablet"
-        //       ? "-20%"
-        //       : "50%",
-        //   scale:
-        //     device === "mobile"
-        //       ? 0.8
-        //       : device === "tablet"
-        //       ? 0.8
-        //       : 1,
-        //   ease: "power1.inOut",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec1",
-        //     start: "top-=60px top+=70px",
-        //     end: "bottom+=100 center",
-        //     scrub: true,
-        //     markers: true
-        //   },
-        // })
-        // .to(productWrapperRef.current, {
-        //   y:
-        //     device === "mobile"
-        //       ? "-8%"
-        //       : device === "tablet"
-        //       ? "-20%"
-        //       : "-25%",
-        //   x:
-        //     device === "mobile"
-        //       ? 0
-        //       : device === "tablet"
-        //       ? 150
-        //       : 330,
-        //   ease: "power1.inOut",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec2",
-        //     start: "center+=100 center+=100",
-        //     end: "center top",
-        //     scrub: true,
-        //   },
-        // });
-        // .to(productWrapperRef.current, {
-        //   y:
-        //     device === "mobile"
-        //       ? "-8%"
-        //       : device === "tablet"
-        //       ? "-20%"
-        //       : "-25%",
-        //   x: device === "mobile" ? 0 : device === "tablet" ? 150 : 330,
-        //   ease: "power1.inOut",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec3",
-        //     start: "top bottom", // When top of the content hits bottom of the viewport
-        //     end: "top center",
-        //     scrub: true,
-        //     // markers: true
-        //   },
-        // });
+          // .to(".productWrapperRef", {
+          //   y:
+          //     device === "mobile"
+          //       ? "90%"
+          //       : device === "tablet"
+          //       ? "-20%"
+          //       : "50%",
+          //   scale:
+          //     device === "mobile"
+          //       ? 0.8
+          //       : device === "tablet"
+          //       ? 0.8
+          //       : 1,
+          //   ease: "power1.inOut",
+          //   scrollTrigger: {
+          //     trigger: ".heroSec1",
+          //     start: "top-=60px top+=70px",
+          //     end: "bottom+=100 center",
+          //     scrub: true,
+          //     markers: true
+          //   },
+          // })
+          // .to(productWrapperRef.current, {
+          //   y:
+          //     device === "mobile"
+          //       ? "-8%"
+          //       : device === "tablet"
+          //       ? "-20%"
+          //       : "-25%",
+          //   x:
+          //     device === "mobile"
+          //       ? 0
+          //       : device === "tablet"
+          //       ? 150
+          //       : 330,
+          //   ease: "power1.inOut",
+          //   scrollTrigger: {
+          //     trigger: ".heroSec2",
+          //     start: "center+=100 center+=100",
+          //     end: "center top",
+          //     scrub: true,
+          //   },
+          // });
+                    // .to(productWrapperRef.current, {
+          //   y:
+          //     device === "mobile"
+          //       ? "-8%"
+          //       : device === "tablet"
+          //       ? "-20%"
+          //       : "-25%",
+          //   x: device === "mobile" ? 0 : device === "tablet" ? 150 : 330,
+          //   ease: "power1.inOut",
+          //   scrollTrigger: {
+          //     trigger: ".heroSec3",
+          //     start: "top bottom", // When top of the content hits bottom of the viewport
+          //     end: "top center",
+          //     scrub: true,
+          //     // markers: true
+          //   },
+          // });
 
         // hand movement
-
-        // gsap.to(handRef.current, {
-        //   y: 1000,
-        //   autoAlpha: 0,
-        //   ease: "power1.inOut",
-        //   scrollTrigger: {
-        //     trigger: ".heroSec1",
-        //     start: "top-=50px top+=35px",
-        //     end: "bottom bottom-=30%",
-        //     scrub: true,
-        //     // markers: true
-        //   },
-        // });
+        
+        gsap.to(handRef.current, {
+          y: 1000,
+          autoAlpha: 0,
+          ease: "power1.inOut",
+          scrollTrigger: {
+            trigger: ".heroSec1",
+            start: "top-=50px top+=35px",
+            end: "bottom bottom-=30%",
+            scrub: true,
+            // markers: true
+          },
+        });
 
         // refresh AFTER all triggers created
         ScrollTrigger.refresh();
@@ -286,8 +247,8 @@ const Hero = () => {
   const line2 = "Less clutter. Better hair. Smarter care.";
 
   return (
-    <section className="relative " ref={containerRef}>
-      <div className="h-full w-full heroMainWrapper">
+    <section className="relative" ref={containerRef}>
+      <div className="h-full w-full">
         <div className=" section h-[calc(100vh-60px)] w-full flex flex-col sm:flex-row items-center justify-between heroSec1">
           {/* <div className="text1 opacity-0 w-full"> */}
           <div className="text1 opacity-0 flex-1">
@@ -328,27 +289,11 @@ const Hero = () => {
             </div>
           </div>
 
-          {/* <div className="flex-1 z-1" ref={productWrapperRef}>
+          <div className="flex-1 z-1" ref={productWrapperRef}>
             <div className="flex flex-col items-center justify-center product-pin-section img">
               <div className="relative lg:w-[600px] lg:h-[600px] md:w-[250px] md:h-[280px] h-[280px] w-[250px] productWrapperRef">
                 <Image
                   
-                  className="object-contain"
-                  src="/bottle1.png"
-                  fill
-                  alt="Product"
-                />
-              </div>
-            </div>
-          </div> */}
-
-          <div className="flex-1 z-1">
-            <div className="flex flex-col items-center justify-center product-pin-section img">
-              <div
-                ref={productWrapperRef}
-                className="relative lg:w-[600px] lg:h-[600px] md:w-[250px] md:h-[280px] h-[280px] w-[250px]"
-              >
-                <Image
                   className="object-contain"
                   src="/bottle1.png"
                   fill
@@ -433,7 +378,7 @@ const Hero = () => {
                   <div className="flex items-center gap-4">
                     <div className="relative rounded-full min-h-[100px] min-w-[100px] md:min-h-[150px] md:min-w-[150px] overflow-hidden">
                       <Image
-                        className="object-cover"
+                      className="object-cover"
                         src={item.img}
                         // width={200}
                         // height={200}
